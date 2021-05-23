ddb0365a5e0e521b408f58e596fef59a4f7fa078
AlexGurov9040 <www.pers93@mail.ru>
2021-05-23 22:15:13 +0300

    Initial commit

---
+++ src/com/gurov/lesson2/task1/Vector.java
@@ -0,0 +1,15 @@
+package com.gurov.lesson2.task1;
+
+public class Vector {
+
+    public static void main(String[] args) {
+
+        Vector app = new Vector();
+        app.startVector();//application start in method startVector
+    }
+
+    public static void startVector(){
+
+        
+    }
+}
